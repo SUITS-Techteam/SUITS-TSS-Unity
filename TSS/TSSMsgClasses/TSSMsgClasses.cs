8c9acf60d5b62f8b56fc5893545e147af27d0508
dilgill <dilgill1234@gmail.com>
2023-03-02 04:52:38 -0600

    Add UIA message type

--- TSS/TSSMsgClasses/TSSMsgClasses.cs
+++ TSS/TSSMsgClasses/TSSMsgClasses.cs
@@ -10,6 +10,7 @@ namespace TSS.Msgs
         public List<GPSMsg> GPS;
         public List<IMUMsg> IMU;
         public List<EVASimState> EVA;
+        public List<UIAMsg> UIA;
     }
 
     [System.Serializable]
@@ -81,6 +82,25 @@ namespace TSS.Msgs
         public string t_oxygen;
         public double cap_water;
         public string t_water;
+        public string createdAt;
+        public string updatedAt;
+    }
+
+    [System.Serializable]
+    public class UIAMsg
+    {
+        public int id;
+        public int room;
+        public string started_at;
+        public bool emu1;
+        public bool ev1_supply;
+        public bool emu1_O2;
+        public bool emu2;
+        public bool ev2_supply;
+        public bool ev_waste;
+        public bool emu2_O2;
+        public bool O2_vent;
+        public bool depress_pump;
         public string createdAt;
         public string updatedAt;
     }
